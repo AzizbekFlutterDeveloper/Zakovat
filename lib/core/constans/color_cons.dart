549c5396d2daf06837826e14169bf7dc126d3564
AzizbekFlutterDeveloper <xxuliganov7@gmail.com>
2022-04-28 15:57:03 +0500

    App Theme finish

---
+++ lib/core/constans/color_cons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+class ConstColor {
+  static Color white = const Color(0xffFFFFFF);
+  static Color blue = const Color(0xff4DB2FF);
+  static Color scaffoldColor = const Color(0xff000416);
+}
